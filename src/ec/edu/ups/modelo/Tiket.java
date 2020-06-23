c763c0a39fbd5d27f7ce1ed2073ccf9658d415ad
Romel_Avila <NANCY@DESKTOP-1JG1RL0>
2020-06-23 12:19:46 -0500

    creacion clase Tiket

--- src/ec/edu/ups/modelo/Tiket.java
+++ src/ec/edu/ups/modelo/Tiket.java
@@ -5,10 +5,91 @@
  */
 package ec.edu.ups.modelo;
 
+import java.util.Date;
+
 /**
  *
  * @author NANCY
  */
 public class Tiket {
+    private int numero;
+    private Date fechaYHoraDeIngreso;
+    private Date fechaYHoraDeSalida;
+    private double total;
+
+    public Tiket() {
+    }
+
+    public Tiket(int numero, Date fechaYHoraDeIngreso, Date fechaYHoraDeSalida, double total) {
+        this.numero = numero;
+        this.fechaYHoraDeIngreso = fechaYHoraDeIngreso;
+        this.fechaYHoraDeSalida = fechaYHoraDeSalida;
+        this.total = total;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public Date getFechaYHoraDeIngreso() {
+        return fechaYHoraDeIngreso;
+    }
+
+    public void setFechaYHoraDeIngreso(Date fechaYHoraDeIngreso) {
+        this.fechaYHoraDeIngreso = fechaYHoraDeIngreso;
+    }
+
+    public Date getFechaYHoraDeSalida() {
+        return fechaYHoraDeSalida;
+    }
+
+    public void setFechaYHoraDeSalida(Date fechaYHoraDeSalida) {
+        this.fechaYHoraDeSalida = fechaYHoraDeSalida;
+    }
+
+    public double getTotal() {
+        return total;
+    }
+
+    public void setTotal(double total) {
+        this.total = total;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 53 * hash + this.numero;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Tiket other = (Tiket) obj;
+        if (this.numero != other.numero) {
+            return false;
+        }
+        return true;
+    }
+
+    
+    
+    @Override
+    public String toString() {
+        return "Tiket{" + "numero=" + numero + ", fechaYHoraDeIngreso=" + fechaYHoraDeIngreso + ", fechaYHoraDeSalida=" + fechaYHoraDeSalida + ", total=" + total + '}';
+    }
+    
     
 }
