e02dbab5f09467f5f44f4e386fb7d3185746ef62
Romel_Avila <NANCY@DESKTOP-1JG1RL0>
2020-06-23 13:03:20 -0500

    creacion clase vehiculo

--- src/ec/edu/ups/modelo/Vehiculo.java
+++ src/ec/edu/ups/modelo/Vehiculo.java
@@ -13,6 +13,67 @@ import java.util.Objects;
  */
 public class Vehiculo {
    
+    private String placa;
+    private String marca;
+    private String modelo;
+
+    public Vehiculo() {
+    }
+
+    public String getPlaca() {
+        return placa;
+    }
+
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 97 * hash + Objects.hashCode(this.placa);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Vehiculo other = (Vehiculo) obj;
+        if (!Objects.equals(this.placa, other.placa)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Vehiculo{" + "placa=" + placa + ", marca=" + marca + ", modelo=" + modelo + '}';
+    }
+    
     
     
 }
