105525071d5801a4d0b7d47da2a17103f8eb3036
braya <braya@BRYEPZ>
2020-06-27 23:13:40 -0500

    Actualizacion del codigo

--- src/ec/edu/ups/vista/VentanaSalidaVehiculo.java
+++ src/ec/edu/ups/vista/VentanaSalidaVehiculo.java
@@ -59,7 +59,7 @@ public class VentanaSalidaVehiculo extends javax.swing.JInternalFrame {
         txtFechaS.setText(local.toString());
     }
     
-    
+     
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
